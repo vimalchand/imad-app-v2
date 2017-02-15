08d89afdf56230a09117e3ac1e335b9d9dae5500
vimalchand <vimalchand23@gmail.com>
2017-02-16 00:19:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,10 @@ app.get('/:articleName',function(req,res){
    res.send(createTemplate(articles[articleName])); 
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
