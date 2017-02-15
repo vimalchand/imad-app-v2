115f04119338957ecf9df3fb9f3c9f5a6e5c7d54
vimalchand <vimalchand23@gmail.com>
2017-02-15 11:52:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',functin(req,res){
+    res.send('Article one requested and will be served here');  
+});
+
+app.get('/article-one',functin(req,res){
+    res.send('Article one requested and will be served here');  
+});
+
+app.get('/article-one',functin(req,res){
+    res.send('Article one requested and will be served here');  
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
