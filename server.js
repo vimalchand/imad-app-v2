47ced843574755a15d3b7f77018724578109644c
vimalchand <vimalchand23@gmail.com>
2017-02-15 19:12:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/article-one',function(req,res){
 
 
 app.get('/article-two',function(req,res){
-    res.send('Article two requested and will be served here');  
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  
 });
 
 app.get('/article-three',function(req,res){
